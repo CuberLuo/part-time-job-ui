6947fd689e921530bd546285e1457b3036b739b1
yangchengchang <yangchengchang2008@163.com>
2022-10-31 20:25:02 +0800

    轮播图 + 兼职广场

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
+    <h1>&ensp;首页</h1>
     <!-- <van-button type="primary" @click="router.push('/login')">登录</van-button>
     <van-button plain type="primary" @click="router.push('/register')"
       >注册</van-button
     > -->
-    index
   </div>
+  <bannerShow></bannerShow>
 </template>
 
 <script setup>
-import router from '@/router'
+import bannerShow from './components/bannerShow.vue'
 </script>
 
 <style scoped></style>
---
+++ src/views/job_square/components/jobCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mycards">
+    <van-card
+      v-for="card in cards"
+      :key="card.id"
+      :price="card.price"
+      currency=""
+      :title="card.content"
+    >
+      <template #tags>
+        <van-tag v-for="(label, index) in card.labels"
+        :key="index"
+        plain type="primary">{{label}}
+        </van-tag>
+      </template>
+      <template #footer>
+        <van-button size="mini">立即报名</van-button>
+      </template>
+    </van-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      cards: []
+    }
+  },
+  methods: {
+
+  },
+  created () {
+    this.cards = [
+      { id: 1, price: '800~1000元', content: '问卷调查', labels: ['热门推荐', '官方渠道', '额外补贴'] },
+      { id: 2, price: '800~1000元', content: '问卷调查', labels: ['热门推荐', '官方渠道', '额外补贴'] },
+      { id: 2, price: '800~1000元', content: '问卷调查', labels: ['热门推荐', '官方渠道', '额外补贴'] }
+    ]
+  }
+}
+</script>
+<style>
+  .mycards{
+    box-shadow: inset;
+  }
+  .van-card{
+    background-color: rgb(119, 146, 244);;
+    border-radius: 10px;
+    box-shadow: 10;
+  }
+  .van-card__title{
+    font-size: 18px;
+    line-height: 30px;
+  }
+  .van-card__price{
+    color: gold;
+    font-size: 10px;
+  }
+</style>
