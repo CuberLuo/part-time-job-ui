464eaeb6ec85b64bc6c2bbd092eacb53b6c64229
CuberLuo <1501858187@qq.com>
2022-11-08 17:49:59 +0800

    可视化图表+消息界面init

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,15 +1,80 @@
 <template>
   <div>
-    <!-- <van-button type="primary" @click="router.push('/login')">登录</van-button>
+    <div>
+      <!-- <van-button type="primary" @click="router.push('/login')">登录</van-button>
     <van-button plain type="primary" @click="router.push('/register')"
       >注册</van-button
     > -->
-    index
+      index
+    </div>
+    <div id="main"></div>
   </div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import router from '@/router'
+import * as echarts from 'echarts'
+
+onMounted(() => {
+  // 基于准备好的dom，初始化echarts实例
+  const myChart = echarts.init(document.getElementById('main'))
+  // 监听图表容器的大小并改变图表大小
+  window.onresize = function () {
+    myChart.resize()
+  }
+  // 绘制图表
+
+  myChart.setOption({
+    legend: {
+      data: ['月累计新增', '总兼职岗位数']
+    },
+    tooltip: {},
+    xAxis: [
+      {
+        type: 'category',
+        data: ['2022-09', '2022-10', '2022-11', '2022-12'],
+        axisPointer: {
+          type: 'shadow'
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        name: '总兼职岗位数',
+        min: 0,
+        max: 250,
+        interval: 50
+      },
+      {
+        type: 'value',
+        name: '月累计新增',
+        min: 0,
+        max: 100,
+        interval: 10
+      }
+    ],
+    series: [
+      {
+        name: '月累计新增',
+        type: 'bar',
+        yAxisIndex: 1,
+        data: [30, 40, 50, 40]
+      },
+      {
+        name: '总兼职岗位数',
+        type: 'line',
+        data: [30, 70, 120, 160]
+      }
+    ]
+  })
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+#main {
+  height: 300px;
+  margin: 0 auto;
+}
+</style>
--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -1,9 +1,77 @@
 <template>
-  <div>message</div>
+  <div v-for="(item, index) in contact" :key="index">
+    <van-swipe-cell>
+      <van-cell title="单元格" class="contact-cell">
+        <template #title>
+          <div>
+            <div class="img-container" style="display: inline-block">
+              <van-image
+                round
+                width="45px"
+                height="45px"
+                :src="item.avatar_src"
+              />
+            </div>
+
+            <div class="contact-info">
+              <div class="contact-title">{{ item.title }}</div>
+              <div class="contact-detail">{{ item.detail }}</div>
+            </div>
+          </div>
+        </template>
+      </van-cell>
+      <template #right>
+        <van-button square type="danger" text="删除" class="delete-button" />
+      </template>
+    </van-swipe-cell>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+const contact = [
+  {
+    avatar_src:
+      'https://img1.baidu.com/it/u=3178057158,4110048229&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=538',
+    title: '客服小助手',
+    detail: '官方客服'
+  },
+  {
+    avatar_src:
+      'https://img2.baidu.com/it/u=2360330494,1092108791&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+    title: '张三',
+    detail: '??'
+  },
+  {
+    avatar_src:
+      'https://img2.baidu.com/it/u=390829681,3002818272&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+    title: '李四',
+    detail: '!!'
+  }
+]
 </script>
 
-<style scoped></style>
+<style scoped>
+.contact-cell {
+  display: flex;
+  border-bottom: 1px solid #dee1e6;
+}
+
+.contact-info {
+  display: inline-block;
+  margin-left: 10px;
+}
+
+.contact-title {
+  font-size: 14px;
+}
+
+.contact-detail {
+  font-size: 12px;
+  color: #999;
+}
+
+.delete-button {
+  height: 100%;
+}
+</style>
